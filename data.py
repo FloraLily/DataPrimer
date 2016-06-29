c12a2cefa955a9d7c76ff779f2369fa5ed8cbd2d
FloraLily <tccr42@mds.ad.dur.ac.uk>
2016-06-29 09:06:59 +0100

    updates 290616
    Ibad's files

--- data.py
+++ data.py
@@ -1,14 +1,54 @@
-import sys
+ import sys 
+ 
+ 
+ f=open("data.csv","r") 
+ analyse=sys.argv[1] 
+ 
 
-f=open("Data.csv","r")
-runningbalance=0
-linecounter=0
-
-for line in f:
-	if linecounter !=0:
-		line=line.strip("\r").strip("\n")
-		entry=line.split(",")
-		runningbalance += int(entry[4])
-	linecounter +=1
-print runningbalance
-f.close
+ runningTotal=0 
+ runningPaid=0 
+ runningBalance=0 
+ 
+ 
+ 
+ 
+ for line in f: 
+ 	line=line.strip('\n').strip('\r') 
+ 	entry=line.split(',') 
+ 	try: 
+ 		runningTotal += int(entry[2]) 
+ 		runningPaid += int(entry[3]) 
+ 		runningBalance += int(entry[4]) 
+	except: 
+ 		pass 
+ 	 
+ print "Total Cost = " + str(runningTotal)  
+ print "Total Paid = " + str(runningPaid) 
+ print "Total Balance = " + str(runningBalance) 
+ 
+ 
+ 
+ 
+ f.seek(0) 
+ counter=0 
+ runningTotal=0 
+ runningPaid=0 
+ runningBalance=0 
+ 
+ 
+ for line in f: 
+ 	line=line.strip('\n').strip('\r') 
+ 	entry=line.split(',') 
+	if entry[5]==analyse: 
+ 		counter+=1 
+ 		print "Clients " + entry[1] + " are " + analyse 
+ 		try: 
+ 			runningTotal += int(entry[2]) 
+ 			runningPaid += int(entry[3]) 
+ 			runningBalance += int(entry[4]) 
+ 		except: 
+ 			pass 
+print str(counter) + " entries are " + analyse 
+print analyse + " Cost = " + str(runningTotal)  
+print analyse + " Paid = " + str(runningPaid) 
+print analyse + " Balance = " + str(runningBalance) 
